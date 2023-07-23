f08563d112a39b73b638ba7eabc808d6075beae1
Olav Bjørlykke <olavbjorlykke@Olavs-MacBook-Air.local>
2023-07-23 15:54:30 +0800

    Added roadlines to the game

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use rusty_engine::prelude::*;
 
 //Constants
 const PLAYER_SPEED: f32 = 250.0;
+const ROAD_SPEED: f32 = 400.0;
+const ROAD_WIDTH: f32 = 300.0;
 
 
 struct GameState {
@@ -31,6 +33,21 @@ fn main() {
     player.layer = 10.0;
     player.collision = true;
 
+    //Adding Roadlines to the game
+    for i in 0..15 {
+        let mut roadline_top = game.add_sprite(format!("Roadline_top{}", i), SpritePreset::RacingBarrierWhite);
+        roadline_top.translation = Vec2::new(-675.0 + 100.0 * (i as f32), ROAD_WIDTH);
+        roadline_top.scale = 0.1;
+
+        let mut roadline_bottom = game.add_sprite(format!("Roadline_bottom{}", i), SpritePreset::RacingBarrierWhite);
+        roadline_bottom.translation = Vec2::new(-675.0 + 100.0 * (i as f32), - ROAD_WIDTH);
+        roadline_bottom.scale = 0.1;
+    }
+
+    for i in 0..15 {
+
+    }
+
     //Adding music to the game
     game.audio_manager.play_music(MusicPreset::WhimsicalPopsicle, 0.5);
 
@@ -58,9 +75,21 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     player1.translation.y += direction * PLAYER_SPEED * engine.delta_f32;
     player1.rotation = 0.15* direction;
 
-    //TODO: Change to be an inline statement
     //Killing the player if it goes out of bounds
-    if player1.translation.y > 360.0 || player1.translation.y < -360.0 {
+    if player1.translation.y > ROAD_WIDTH || player1.translation.y < - ROAD_WIDTH {
         game_state.health = 0;
+    }
+
+    //Moving roadlines if right arrow is pressed to create illusion of movement to the right
+    if engine.keyboard_state.pressed(KeyCode::Right){
+        for sprite in engine.sprites.values_mut() {
+            if sprite.label.starts_with("Roadline") {
+                sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+                if sprite.translation.x < -675.0 {
+                    //Moving Roadlines to the other side when they pass outside the screen
+                    sprite.translation.x += 1500.0
+                }
+            }
+        }
     }
 }
