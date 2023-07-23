1ec84e024f69a51828eee702add8e29abef96941
Olav Bjørlykke <olavbjorlykke@Olavs-MacBook-Air.local>
2023-07-23 15:03:11 +0800

    Added player movement functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+//Libraries
 use rusty_engine::prelude::*;
+
+//Constants
+const PLAYER_SPEED: f32 = 250.0;
 
 
 struct GameState {
@@ -38,4 +42,25 @@ fn main() {
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     let mut direction:f32 = 0.0;
+
+    //Fetching Keyboard inputs
+    if engine.keyboard_state.pressed(KeyCode::Up){
+        direction += 1.0;
+    }
+    if engine.keyboard_state.pressed(KeyCode::Down){
+        direction -= 1.0;
+    }
+
+    //Getting mutable reference to the player
+    let player1 = engine.sprites.get_mut("Player_1").unwrap();
+
+    //Moving the player with input
+    player1.translation.y += direction * PLAYER_SPEED * engine.delta_f32;
+    player1.rotation = 0.15* direction;
+
+    //TODO: Change to be an inline statement
+    //Killing the player if it goes out of bounds
+    if player1.translation.y > 360.0 || player1.translation.y < -360.0 {
+        game_state.health = 0;
+    }
 }
